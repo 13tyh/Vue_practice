5c324101a71a4053b3749b80ddfd32a3cdf2a250
13tyh <yuck5g@gmail.com>
2024-11-27 14:52:12 +0900

    キー修飾子

---
+++ src/App4.vue
@@ -0,0 +1,15 @@
+<script setup name="App4">
+import { ref } from 'vue'
+</script>
+<template>
+  //キーボード系のイベント
+  <p>{{ count }}</p>
+  <input type="text" @keyup="count++" />
+  // キーが上がった時に発火するイベント
+
+  <input type="text" @keydown="count++" />
+  // キーが下がった時に発火するイベント
+
+  <input type="text" @keyup.space="count++" />
+  // スペースキーが上がった時のみに発火するイベント
+</template>
