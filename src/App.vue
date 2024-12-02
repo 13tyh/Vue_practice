c9aebbf3bf3d45d37d2f7a3c86d830cd39a259f0
13tyh <yuck5g@gmail.com>
2024-12-03 00:52:36 +0900

    v-ifとv-forの併用,オブジェクトに対してv-forを使う方法

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,20 @@
 <script setup>
 import { ref } from 'vue'
-const ok = ref(true)
-const maybe = ref(false)
-
 const fruits = ref(['apple', 'banana', 'cherry'])
+const user = ref({
+  name: 'Yoshipi',
+  age: 25,
+  gender: 'man',
+})
 </script>
 <template>
-  <button @click="ok = !ok">toggle</button>
-  <p v-if="ok">OK!</p>
-  <p v-else-if="maybe">Maybe...OK!</p>
-  <p v-else-if="maybe">Maybe...OK!</p>
-  <p v-else-if="maybe">Maybe...OK!</p>
-  <p v-else-if="maybe">Maybe...OK!</p>
-  // falseの時は下に処理が行く // v-if, v-else-if, v-elseを並べないとエラーになる
-
-  <template v-if="ok">
-    <p>OK!</p>
-    <p>Hello</p>
-    <p>Hi</p>
+  <template v-for="(fruit, index) in fruits" :key="fruit">
+    <p>type="text">{{ fruit }},{{ index }}</p>
   </template>
-  // v-ifの中に複数の要素を持たせる場合はtemplateタグで囲む //
-  完全に消されるので、templateタグはhtmlには出力されない // 高頻度で切り替えない場合はv-ifを使う
+  // v-ifとv-forは同じ要素に使うことができない // // 使う場合はtemタグを増やして一つずらす
 
   <template>
-    <p v-show="ok">Ok!</p>
-  </template>
-  // v-showはdisplay: none;で非表示にするだけ //
-  処理が早いので、頻繁に表示非表示を切り替える場合はv-showを使う
-
-  <template>
-    <li v-for="(fruit, index) in fruits" :key="fruit">
-      //indexをkey属性にするのは非推奨 // key属性はユニークな値を指定する //
-      <input type="text">{{ fruit }},{{ index }}</input>
-    </li>
-    // v-forで配列の中身を表示する // :key="fruit"でユニークなkeyを指定する //
-    appleが被ったら行けないので、ユニークなkeyを指定する(オブジェクトにしてidなど)
-    // ({})の中だと分割代入ができる
+    <p v-for="(value, key, index) in user" :key="value">{{ key }}:{{ value }}({{ index }})</p>
   </template>
 </template>
 <style></style>
