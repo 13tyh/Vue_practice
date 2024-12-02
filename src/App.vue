60fc46650c613b601839b8b3c107912041b0e1d4
13tyh <yuck5g@gmail.com>
2024-12-03 00:54:38 +0900

    n in 数字  の時の挙動

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <script setup>
 import { ref } from 'vue'
-const fruits = ref(['apple', 'banana', 'cherry'])
-const user = ref({
-  name: 'Yoshipi',
-  age: 25,
-  gender: 'man',
-})
 </script>
 <template>
-  <template v-for="(fruit, index) in fruits" :key="fruit">
-    <p>type="text">{{ fruit }},{{ index }}</p>
-  </template>
-  // v-ifとv-forは同じ要素に使うことができない // // 使う場合はtemタグを増やして一つずらす
-
-  <template>
-    <p v-for="(value, key, index) in user" :key="value">{{ key }}:{{ value }}({{ index }})</p>
-  </template>
+  <p v-for="n in 10" :key="n">{{ n }}</p>
+  // indexが0スタートではなく1スタートになる
 </template>
 <style></style>
