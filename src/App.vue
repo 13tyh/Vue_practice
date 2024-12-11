8cd356752ea05140a0fad05b07d80a6f195df821
13tyh <yuck5g@gmail.com>
2024-12-11 15:05:37 +0900

    propsの命名規則

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,6 @@ const count = ref(0)
 </script>
 <template>
   <ShowCount :foo="count" bar="hello" />
+  {/* propsの命名規則はケバブケース */}
   <button @click="count++">+1</button>
 </template>
--- src/components/ShowCount.vue
+++ src/components/ShowCount.vue
@@ -2,10 +2,7 @@
 defineProps({
   foo: { Number, default: 100 },
   bar: { String, required: true },
-}) // デフォルトとrequiredを設定することができる
-// 配列とオブジェクトの時だけは、defaultを使うことができない
-// typeがbooleanの時は、undefinedを設定することができず、trueになる
-// データがないときはfalseになる
+})
 </script>
 
 <template>
