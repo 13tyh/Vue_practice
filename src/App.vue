6fb6749e7d196312e926efeb23ab6b3310736fea
13tyh <yuck5g@gmail.com>
2024-12-11 15:24:50 +0900

    一旦リセット

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,2 @@
-<script setup>
-import { ref } from 'vue'
-import ResetButton from './components/ResetButton.vue'
-const count = ref(0)
-// function onReset(value) {
-// count.value = value
-// }
-</script>
-<template>
-  <p>count: {{ count }}</p>
-  <button @click="count++">+1</button>
-  <ResetButton @reset="count = $event" />
-  <!-- <ResetButton @reset="onReset" /> -->
-</template>
+<script setup></script>
+<template></template>
