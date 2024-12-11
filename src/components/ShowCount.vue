0d2082c0d35a695157b5c5c37b5406a95197758d
13tyh <yuck5g@gmail.com>
2024-12-11 14:47:58 +0900

    propsの渡し方

---
+++ src/components/ShowCount.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(['foo', 'bar'])
+// const { foo, bar } = defineProps(['foo', 'bar'])
+//この書き方でも良い（分割代入）
+console.log(props.foo)
+// propsは読み取り専用なので、以下のような書き方はできない
+// props.foo = 10
+// 親コンポーネントでpropsを更新する
+</script>
+
+<template>
+  <p>count: {{ foo }}</p>
+  <p>{{ bar }}</p>
+</template>
