43fb09063a901a624c4d7417d7d198b3c818e678
13tyh <yuck5g@gmail.com>
2024-12-13 01:21:26 +0900

    KeepAliveにつけれる属性とKeepAliveを使った時のライフサイクル

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
-import { shallowRef } from 'vue'
+import { onActivated, onDeactivated } from 'vue'
 
-const currentComp = shallowRef(CompA)
+onActivated(() => {
+  console.log('activated')
+})
+onDeactivated(() => {
+  console.log('deactivated')
+})
+
+// KeepAliveの時のライフサイクル
 </script>
 <template>
   <h1>動的コンポーネント</h1>
-  <component :is="currentComp" />
-  // ボタンをクリックするとcurrentComponentの値が変わる // 例：タブの切り替え //
-  shallowRefを使ってcurrentComponentの値を変更する // パフォーマンスがrefよりも良い
+  <KeepAlive>
+    <component :is="currentComp" />
+  </KeepAlive>
+  // KeepAliveは動的コンポーネントの状態を保持する // 動的コンポーネントと一緒に使う //
+  v-showでも同じことが可能 // includeで指定したコンポーネントのみデータを保存する //
+  excludeで指定したコンポーネントのデータを保存しない // maxで保存するデータの数を指定する
 </template>
