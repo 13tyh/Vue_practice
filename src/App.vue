617c7478e3b1cdeeb6b964754e9f1c8145b5d11d
13tyh <yuck5g@gmail.com>
2024-12-13 11:26:50 +0900

    Teleport

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
-<script setup>
-import { onActivated, onDeactivated } from 'vue'
-
-onActivated(() => {
-  console.log('activated')
-})
-onDeactivated(() => {
-  console.log('deactivated')
-})
-
-// KeepAliveの時のライフサイクル
-</script>
+<script setup></script>
 <template>
-  <h1>動的コンポーネント</h1>
-  <KeepAlive>
-    <component :is="currentComp" />
-  </KeepAlive>
-  // KeepAliveは動的コンポーネントの状態を保持する // 動的コンポーネントと一緒に使う //
-  v-showでも同じことが可能 // includeで指定したコンポーネントのみデータを保存する //
-  excludeで指定したコンポーネントのデータを保存しない // maxで保存するデータの数を指定する
+  <Teleport to="body">
+    <h2>aaa</h2>
+  </Teleport>
+  // to属性で指定した要素の一番下に挿入される // モーダルに使われる時が多い // disable属性をつけれる
+  //deferをつけると全てのコンポーネントがマウントされた後移動する
 </template>
