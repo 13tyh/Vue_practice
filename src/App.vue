16db99c35e23ecd7dcf4d5324c8244303cfb0470
13tyh <yuck5g@gmail.com>
2024-12-13 11:48:10 +0900

    非同期コンポーネントを遅延読み込みする方法、ローディング中やエラー時に別のコンポーネントを表示する方法

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
-<script setup></script>
+<script setup>
+import { defineAsyncComponent } from 'vue'
+
+// const CompB = defineAsyncComponent(() => import('./components/CompB.vue'))
+// 初回で全てダウンロードされずに、CompB.vue が表示されるまでダウンロードされない // 初回レンダリンングスピードやパフォーマンスが上がる
+// ボタンを押してからのレンダリングが遅い
+
+const CompB = defineAsyncComponent({
+  loader: () => import('./components/CompB.vue'),
+  loadingComponent: {
+    template: '<div>loading...</div>',
+  },
+  delay: 2000, //デフォルト
+  errorComponent: {
+    template: '<div>error...</div>',
+  },
+  timeout: 3000,
+})
+
+//loadingComponentは、遅いレンダリングの間に表示されるコンポーネントを指定する
+//ローディング画面を入れるような時に使う
+//delayはloadingComponentが表示されるまでの時間
+//errorComponentは、エラーが発生した時に表示されるコンポーネントを指定する
+//timeoutは、エラーが発生した時にerrorComponentが表示されるまでの時間
+// era0
+</script>
 <template>
-  <Teleport to="body">
-    <h2>aaa</h2>
-  </Teleport>
-  // to属性で指定した要素の一番下に挿入される // モーダルに使われる時が多い // disable属性をつけれる
-  //deferをつけると全てのコンポーネントがマウントされた後移動する
+  <h1>Component</h1>
+  <CompB />
 </template>
